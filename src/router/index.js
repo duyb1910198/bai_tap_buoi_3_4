1d39dd888af199272e52f381c9a7d6865c0e7737
Anh Duy <duygolll@gmail.com>
2023-03-21 13:21:03 +0700

    Tao trang them lien he

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,11 @@ const routes = [
         props: true
         // Truyền các biến trong $route.params vào làm props
     },
+    {
+        path: "/contacts/add",
+        name: "contact.add",
+        component: () => import("@/views/ContactCreate.vue"),
+    },
 ];
 
 const router = createRouter({
